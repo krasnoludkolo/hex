6faa0c6951a61a41845701a6d871c1b026e39cbf
janek@projmen.pl <janek@projmen.pl>
2020-08-07 19:18:12 +0200

    Refactor

--- hex-rules/src/main/kotlin/hex/GameStatus.kt
+++ hex-rules/src/main/kotlin/hex/GameStatus.kt
@@ -3,6 +3,9 @@ package hex
 import io.vavr.collection.List
 
 sealed class GameStatus {
+
+    abstract val board: Board
+
     companion object {
         fun ended(board: Board, winner: HexPlayer, history: List<Move>) = EndedGameStatus(board, winner, history)
         fun ongoing(board: Board, winner: HexPlayer) = OngoingGameStatus(board, winner)
@@ -10,12 +13,12 @@ sealed class GameStatus {
 }
 
 data class EndedGameStatus(
-    val board: Board,
+    override val board: Board,
     val winner: HexPlayer,
     val history: List<Move>
 ) : GameStatus()
 
 data class OngoingGameStatus(
-    val board: Board,
+    override val board: Board,
     val activeHexPlayer: HexPlayer
 ) : GameStatus()
